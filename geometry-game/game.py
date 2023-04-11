b5ce37a2494fb595b9336cbe2dcf324ffbdd8706
Raymond Oluoch <rasholuoch@gmail.com>
2023-04-11 16:04:35 +0300

    refactor and code improvement

--- geometry-game/game.py
+++ geometry-game/game.py
@@ -1,20 +1,45 @@
+from random import randint
+
 class Point:
 
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-  def falls_in_rectangle(self, lowleft, upright):
-    if lowleft[0] < self.x < upright[0] \
-    and lowleft[1] < self.y < upright[1]:
-      return True
-    else:
-      return False
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+                and rectangle.point1.y < self.y < rectangle.point2.y:
+            return True
+        else:
+            return False
 
-  def distance_from_point(self, point):
-    return ((self.x - point.x)**2 +
-            (self.y - point.y)**2) ** 0.5
 
-point1 = Point(1,1)
-point2 = Point(2,2)
-print(point1.distance_from_point(point2))
+class Rectangle:
+
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+               (self.point2.y - self.point1.y)
+
+
+# Create rectangle object
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
+              Point(randint(10, 19), randint(10, 19)))
+
+# Print rectangle coordinates
+print("Rectangle Coordinates: ",
+      rectangle.point1.x, ",",
+      rectangle.point1.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
+
+# Get point and area from user
+user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+
+# Print out the game result
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
