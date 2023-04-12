e144475cdc15f06aa08b605c7297d512c8eb9dce
Raymond Oluoch <rasholuoch@gmail.com>
2023-04-12 19:01:13 +0300

    implementation phase

--- flatmates-bill/main.py
+++ flatmates-bill/main.py
@@ -1,3 +1,8 @@
+from fpdf import FPDF
+import os
+import webbrowser
+
+
 class Bill:
     """
     Object that contains data about a bill, such as total amount and period of the bill.
@@ -17,8 +22,10 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        pass
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 
 class PdfReport:
@@ -30,4 +37,44 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation="P", unit="pt", format="A4")
+        pdf.add_page()
+
+        # Add icon
+        pdf.image("flatmates-bill/house.png", w=30, h=30)
+
+        # Insert title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Insert Period lable and value
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=100, h=40, txt="Period", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount of the first flatmate
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0)
+
+        pdf.output(self.filename)
+
+        webbrowser.open('file://' + os.path.realpath(self.filename))
+
+
+the_bill = Bill(amount=120, period="April 2023")
+john = Flatmate(name="John", days_in_house=20)
+mary = Flatmate(name="Mary", days_in_house=25)
+
+print("John Pays: ", john.pays(bill=the_bill, flatmate2=mary))
+print("Mary Pays: ", mary.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="flatmates-bill/Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=mary, bill=the_bill)
