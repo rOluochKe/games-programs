9a770c3e47c6b8198b8957a9a2af0afaaca2a641
Raymond Oluoch <rasholuoch@gmail.com>
2023-04-14 12:53:42 +0300

    methods implementation

--- math-painter/main.py
+++ math-painter/main.py
@@ -1,34 +1,59 @@
-class Canvas:
+from PIL import Image
+import numpy as np
 
-    def __init__(self, a, b, color):
-        self.a = a
-        self.b = b
+
+class Canvas:
+    """Object where all shapes are going to be drawn"""
+
+    def __init__(self, height, width, color):
+        self.height = height
+        self.width = width
         self.color = color
 
-    def make(self):
-        pass
+        # Create a 3d numpy array of zeros
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        # Change [0,0,0] with user given values for color
+        self.data[:] = self.color
+
+    def make(self, imagepath):
+        """Converts the current array into an image file"""
+        img = Image.fromarray(self.data, 'RGB')
+        img.save(imagepath)
 
 
 class Rectangle:
+    """A rectangle shape that can be drawn on a Canvas object"""
 
-    def __init__(self, x, y, a, b, color):
+    def __init__(self, x, y, height, width, color):
         self.x = x
         self.y = y
-        self.a = a
-        self.b = b
+        self.height = height
+        self.width = width
         self.color = color
 
     def draw(self, canvas):
-        pass
-
+        """Draws itself into the canvas"""
+        # Changes a slice of the array with new values
+        canvas.data[self.x: self.x + self.height, self.y: self.y + self.width] = self.color
 
 class Square:
+    """A square shape that can be drawn on a canvas object"""
 
-    def __init__(self, x, y, a, color):
+    def __init__(self, x, y, side, color):
         self.x = x
         self.y = y
-        self.a = a
+        self.side = side
         self.color = color
 
     def draw(self, canvas):
-        pass
+        """Draws itself into the canvas"""
+        # Changes a slice of the array with new values
+        canvas.data[self.x: self.x + self.side, self.y: self.y + self.side] = self.color
+
+
+canvas = Canvas(height=20, width=30, color=(255, 255, 255))
+r1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 125))
+r1.draw(canvas)
+s1 = Square(x=1, y=3, side=3, color=(0, 100, 222))
+s1.draw(canvas)
+canvas.make('math-painter/images/canvas.png')
